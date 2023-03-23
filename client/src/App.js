900815fec6d4aed9c0d2b9832398df37578eabce
WASZKA009 <oskiwaszka@gmail.com>
2023-03-23 17:35:03 +0100

    Building up admin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import './App.css';
+import './App.scss';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import io from 'socket.io-client';
 import { useEffect } from 'react';
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-
-
   return (
-    <div className="App">
-      <table>
-        <tr >
-          <td>
-
-          </td>
-        </tr>
-      </table>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col className='column'>
+          <p>Siema</p>
+        </Col>
+        <Col className='column'>
+          <p>Siema</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='column'>
+          <p>Siema</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
